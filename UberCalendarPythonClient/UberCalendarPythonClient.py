fe01e8a680df514f4cbfaf8e2f8e7a3ee7102727
Igor B <igorbienkowski@gmail.com>
2019-01-31 19:21:51 +0000

    Straight into gui learning with tkinter

--- UberCalendarPythonClient/UberCalendarPythonClient.py
+++ UberCalendarPythonClient/UberCalendarPythonClient.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+from tkinter import *
+
+root = Tk()
+topFrame = Frame(root)
+topFrame.pack(side=TOP)
+
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+button1 = Button(topFrame, text="Button 1", fg="red")
+button2 = Button(topFrame, text="Button 2", fg="blue")
+button3 = Button(topFrame, text="Button 3", fg="green")
+button4 = Button(bottomFrame, text="Button 4", fg="yellow")
+
+button1.pack(side=LEFT)
+button2.pack(side=LEFT)
+button3.pack(side=LEFT)
+button4.pack()
+
+root.mainloop()
